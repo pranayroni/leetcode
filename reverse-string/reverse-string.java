d19591ffde8b1063ecd7f6da228b1c9d789a2ee6
Pranay Roni <pranay.roni@icloud.com>
2022-10-18 23:33:23 -0400

    Time: 1 ms, Memory: 54.2 MB - LeetHub

--- reverse-string/reverse-string.java
+++ reverse-string/reverse-string.java
@@ -2,14 +2,14 @@ class Solution {
     public void reverseString(char[] s) {
         int left =0;
         int right = s.length-1;
-        
-        while(left<right)
-        {
+        while(left<right){
             char tmp = s[left];
             s[left] = s[right];
             s[right] = tmp;
+            
             left++;
             right--;
+            
         }
     }
 }
