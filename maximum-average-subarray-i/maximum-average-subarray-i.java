aca735db8e898deddf920dcee2beef0ab2f08051
Pranay Roni <pranay.roni@icloud.com>
2022-10-18 23:42:50 -0400

    Time: 22 ms, Memory: 117.6 MB - LeetHub

--- maximum-average-subarray-i/maximum-average-subarray-i.java
+++ maximum-average-subarray-i/maximum-average-subarray-i.java
@@ -2,18 +2,21 @@ class Solution {
     public double findMaxAverage(int[] nums, int k) {
         int left =0;
         int right =0;
-        int sum=0;
-        Double avg =null;
-        
+        int sum =0;
+        Double avg = null;
         while(left<nums.length && right<nums.length)
         {
             sum+=nums[right];
-            if((right-left)+1>k){
+            if((right-left)+1 >k)
+            {
                 sum-=nums[left];
                 left++;
             }
-            if((right-left)+1==k && avg!=null) avg = Math.max(avg,sum/(double)k);
-            else if((right-left)+1==k && avg == null) avg = sum/(double)k;
+            if((right-left)+1 == k)
+            {
+                if(avg==null) avg = sum/(double)k;
+                else avg = Math.max(avg,sum/(double)k);
+            }
             right++;
         }
         return avg;
