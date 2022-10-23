d27c61bfc8fd3a9b895ab490744a53768cf69a5e
Pranay Roni <pranay.roni@icloud.com>
2022-10-23 18:51:59 -0400

    Time: 151 ms (25.13%), Space: 86.6 MB (12.68%) - LeetHub

---
+++ 1472-design-browser-history/1472-design-browser-history.java
@@ -0,0 +1,60 @@
+class BrowserHistory {
+
+    Deque<String> back = new ArrayDeque<>();
+    Deque<String> forward = new ArrayDeque<>();
+
+    String curr;
+    public BrowserHistory(String homepage) {
+        curr = homepage;
+    }
+    
+    public void visit(String url) {
+        back.push(curr);
+        curr = url;
+        forward = new ArrayDeque<>();
+    }
+    
+    public String back(int steps) {
+        if(steps>back.size())
+        {
+            while(!back.isEmpty())
+            {
+                forward.push(curr);
+                curr = back.pop();
+            }
+            return curr;
+        }
+        for(int i=0; i<steps; i++) 
+        {
+            forward.push(curr);
+            curr = back.pop();            
+        }
+        return curr;
+    }
+    
+    public String forward(int steps) {
+        if(steps>forward.size())
+        {
+            while(!forward.isEmpty())
+            {
+                back.push(curr);
+                curr = forward.pop();
+            }
+            return curr;
+        }
+        for(int i=0; i<steps; i++) 
+        {
+            back.push(curr);
+            curr = forward.pop();            
+        }
+        return curr;
+    }
+}
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory obj = new BrowserHistory(homepage);
+ * obj.visit(url);
+ * String param_2 = obj.back(steps);
+ * String param_3 = obj.forward(steps);
+ */
