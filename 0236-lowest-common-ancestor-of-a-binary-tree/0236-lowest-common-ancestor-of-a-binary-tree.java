f7ccddc6407ddb1338c8b7d0713efceca3ed25d7
Pranay Roni <pranay.roni@icloud.com>
2022-10-28 02:46:43 -0400

    Time: 41 ms (5.34%), Space: 49.2 MB (5.90%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -9,43 +9,35 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        // make hashmap of all nodes and their parents
-        // put all of ps ancestors in a set
-        // while q is not in that set, we go up qs ancestors until we find a common match
-        // when match is found we return that q
-        
-        Map<TreeNode,TreeNode> parents = new HashMap<>();
-        Deque<TreeNode> s= new ArrayDeque<>();
-        parents.put(root,null);
+        Map<TreeNode,TreeNode> hm = new HashMap<>();
+        Deque<TreeNode> s = new ArrayDeque<>();
+        hm.put(root,null);
         s.push(root);
-        while(!parents.containsKey(p) || !parents.containsKey(q))
+        
+        while(!(hm.containsKey(p) && hm.containsKey(q)))
         {
             TreeNode node = s.pop();
             if(node.left!=null)
             {
-                parents.put(node.left,node);
-                s.push(node.left);
+                hm.put(node.left,node);
+                s.add(node.left);
             }
             if(node.right!=null)
             {
-                parents.put(node.right,node);
-                s.push(node.right);
+                hm.put(node.right,node);
+                s.add(node.right);
             }
-            
         }
-        
-        Set<TreeNode> set = new HashSet<>();
+        Set<TreeNode> a = new HashSet<>();
         while(p!=null)
         {
-            set.add(p);
-            p = parents.get(p);
+            a.add(p);
+            p = hm.get(p);
         }
-        while(!set.contains(q))
+        while(!a.contains(q))
         {
-            q = parents.get(q);
+            q = hm.get(q);
         }
         return q;
-        
-        
-        }
+    }
 }
